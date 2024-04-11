a04a5615cd3a7e4d00712abd03da60baf45cf931
marinho777 <danielmarinhom@gmail.com>
2024-04-11 12:17:49 -0300

    chore:  first commit

---
+++ Provinha/Provinha/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System;
+
+namespace Provinha
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            PickerDice.SelectedIndex = 0;
+        }
+
+        private void RollDice_Clicked(object sender, EventArgs e) 
+        {
+            int selected = Convert.ToInt32(PickerDice.SelectedIndex);
+            int tipo = Convert.ToInt32(PickerDice.Items[selected]);
+            Random random = new Random();
+            int aleatorio = random.Next(tipo);
+            while(aleatorio == 0)
+            {
+                aleatorio = random.Next(tipo);
+            }
+            LabelDice.Text = aleatorio.ToString();
+        }
+
+}
+
+}
